b11073f2f97b6734dd49810efb10b5ad2e61a276
Matthew Kerwin <matthew@kerwin.net.au>
2013-04-07 23:53:13 +1000

    added number parsing
    also added most number parsing tests

--- parser.rb
+++ parser.rb
@@ -38,6 +38,18 @@ class MarblParser
 			str = parse_number str, tree
 		when /[[:word:]]/
 			str = parse_word str, tree
+		when '-'
+			if str[1,1] =~ /[[:digit:]]/
+				str = parse_negative_number str[1..-1], tree
+			else
+				str = parse_symbol str, tree
+			end
+		when '+'
+			if str[1,1] =~ /[[:digit:]]/
+				str = parse_number str[1..-1], tree
+			else
+				str = parse_symbol str, tree
+			end
 		else
 			str = parse_symbol str, tree
 		end
@@ -97,6 +109,15 @@ class MarblParser
 		str
 	end
 
+	def parse_negative_number str, tree
+		tmp = []
+		str = parse_number str[1..-1], tmp
+		token = tmp[0]
+		token[:value] = -token[:value]
+		tree << token
+		str
+	end
+
 	def parse_number str, tree
 		case str[0,1]
 		when '0'
@@ -105,25 +126,252 @@ class MarblParser
 				parse_hex str[2..-1], tree
 			when 'b', 'B'
 				parse_binary str[2..-1], tree
-			when 'o', 'O', /[0-7]/
+			when 'o', 'O'
 				parse_octal str[2..-1], tree
+			when /[0-7]/
+				parse_octal str[1..-1], tree
+			when '', /[^[:word:]]/
+				tree << MarblToken.new( :integer, 0 )
+				str[1..-1]
 			else
-				throw MarblParseError, "invalid number format #{c}"
+				raise MarblParseError, "invalid number format #{c}"
 			end
 		when /[1-9]/
 			parse_decimal str, tree
 		else
-			tree << MarblToken.new( :number_decimal, 0 )
+			raise MarblParseError, "unexpected `#{c}' in number"
 		end
 	end
 
+	def parse_hex str, tree
+		_parse_number 16, /[0-9A-F]/i, str, tree
+	end
+
 	def parse_decimal str, tree
-		# while str[0] in '0'..'9' or '_', or str[0] == '_', or str[0] == '.' {and go into float mode}, or str[0] == 'e' and in float mode ...
-		
+		_parse_number 10, /[0-9]/, str, tree
 	end
 
 	def parse_octal str, tree
-		# ... (error on 8,9)
+		_parse_number 8, /[0-7]/, str, tree
 	end
 
+	def parse_binary str, tree
+		_parse_number 2, /[01]/, str, tree
+	end
+
+	def _parse_number base, digit, str, tree
+		n = ''
+		until str.empty?
+			case c=str[0,1]
+#			when '_'
+			when digit
+				n << c
+			when '.'
+				if str[1,1] =~ digit
+					return _parse_float( n, base, digit, str[1..-1], tree )
+				else
+					break
+				end
+			when 'e', 'E'
+				return _parse_float_decexp( n.to_i(base), 1, str[1..-1], tree)
+			when 'p', 'P'
+				return _parse_float_binexp( n.to_i(base), 1, str[1..-1], tree)
+			when /[^[:word:]]/
+				break
+			else
+				raise MarblParseError, "unexpected `#{c}' in number"
+			end
+			str = str[1..-1]
+		end
+		tree << MarblToken.new( :integer, n.to_i(base) )
+		str
+	end
+	def _parse_float n, base, digit, str, tree
+		m = ''
+		until str.empty?
+			case c=str[0,1]
+#			when '_'
+			when digit
+				m << c
+			when 'e', 'E'
+				return _parse_float_decexp( (n+m).to_i(base), base ** m.length, str[1..-1], tree )
+			when 'p', 'P'
+				return _parse_float_binexp( (n+m).to_i(base), base ** m.length, str[1..-1], tree )
+			when /[^[:word:]]/
+				break
+			else
+				raise MarblParseError, "unexpected `#{c}' in number"
+			end
+			str = str[1..-1]
+		end
+		numer = (n + m).to_i(base)
+		denom = base ** m.length
+		tree << MarblToken.new( :float, Rational(numer, denom) )
+		str
+	end
+	def _parse_float_decexp n, b, str, tree
+		x = ''
+		neg = nil
+		until str.empty?
+			case c=str[0,1]
+			when '-'
+				raise MarblParseError, "unexpected `#{c}' in number" unless neg.nil?
+				neg = true
+			when '+'
+				raise MarblParseError, "unexpected `#{c}' in number" unless neg.nil?
+				neg = false
+			when /[0-9]/
+				neg = false if neg.nil?
+				x << c
+			when /[^[:word:]]/
+				break
+			else
+				raise MarblParseError, "unexpected `#{c}' in number"
+			end
+			str = str[1..-1]
+		end
+		x = neg ? x.to_i : -x.to_i
+		denom = b * (10 ** x)
+		tree << MarblToken.new( :float, Rational(n, denom) )
+		str
+	end
+	def _parse_float_binexp n, b, str, tree
+		x = ''
+		neg = nil
+		until str.empty?
+			case c=str[0,1]
+			when '-'
+				raise MarblParseError, "unexpected `#{c}' in number" unless neg.nil?
+				neg = true
+			when '+'
+				raise MarblParseError, "unexpected `#{c}' in number" unless neg.nil?
+				neg = false
+			when /[0-9]/
+				neg = false if neg.nil?
+				x << c
+			when /[^[:word:]]/
+				break
+			else
+				raise MarblParseError, "unexpected `#{c}' in number"
+			end
+			str = str[1..-1]
+		end
+		x = neg ? x.to_i : -x.to_i
+		denom = b * (2 ** x)
+		tree << MarblToken.new( :float, Rational(n, denom) )
+		str
+	end
+
+end
+
+
+$parser = MarblParser.new
+def parse_num n
+	t = []
+	s = $parser.parse_number( n, t )
+	[s, t[0].value.to_f]
+end
+
+require 'test/unit'
+class Test_parser < Test::Unit::TestCase
+	def test_dec_ints
+		assert_equal( ['',0.0], parse_num( '0' ))
+		assert_equal( ['',1.0], parse_num( '1' ))
+		assert_equal( [' ',12.0], parse_num( '12 ' ))
+		assert_equal( [' 3',12.0], parse_num( '12 3' ))
+		assert_equal( ['.foo',12.0], parse_num( '12.foo' ))
+	end
+	def test_dec_floats
+		assert_equal( ['',1.2], parse_num( '1.2' ))
+		assert_equal( ['',1200.0], parse_num( '1.2e3' ))
+		assert_equal( ['',0.0012], parse_num( '1.2e-3' ))
+		assert_equal( ['',10.0], parse_num( '1e1' ))
+		assert_equal( ['',0.1], parse_num( '1e-1' ))
+		assert_equal( ['',2.0], parse_num( '1p1' ))
+		assert_equal( ['',0.5], parse_num( '1p-1' ))
+
+		assert_equal( ['.foo',1.2], parse_num( '1.2.foo' ))
+		assert_equal( ['.foo',1200.0], parse_num( '1.2e3.foo' ))
+		assert_equal( ['.foo',0.0012], parse_num( '1.2e-3.foo' ))
+		assert_equal( ['.foo',10.0], parse_num( '1e1.foo' ))
+		assert_equal( ['.foo',0.1], parse_num( '1e-1.foo' ))
+		assert_equal( ['.foo',2.0], parse_num( '1p1.foo' ))
+		assert_equal( ['.foo',0.5], parse_num( '1p-1.foo' ))
+	end
+	def test_ints
+		assert_equal( ['',16.0], parse_num( '0x10' ))
+		assert_equal( ['',2.0], parse_num( '0b10' ))
+		assert_equal( ['',8.0], parse_num( '0o10' ))
+		assert_equal( ['',8.0], parse_num( '010' ))
+	end
+	def test_floats
+		assert_equal( ['',16.0625], parse_num( '0x10.1' ))
+		assert_equal( ['',2.5], parse_num( '0b10.1' ))
+		assert_equal( ['',8.125], parse_num( '0o10.1' ))
+		assert_equal( ['',8.125], parse_num( '010.1' ))
+	end
+	def test_floats_imp
+		assert_equal( ['',4321.0], parse_num( '0x10e1' )) # quirk!
+		assert_equal( ['',20.0], parse_num( '0b10e1' ))
+		assert_equal( ['',80.0], parse_num( '0o10e1' ))
+		assert_equal( ['',80.0], parse_num( '010e1' ))
+
+		assert_equal( ['',32.0], parse_num( '0x10p1' ))
+		assert_equal( ['',4.0], parse_num( '0b10p1' ))
+		assert_equal( ['',16.0], parse_num( '0o10p1' ))
+		assert_equal( ['',16.0], parse_num( '010p1' ))
+
+		assert_equal( ['',16.117431640625], parse_num( '0x10.1e1' )) # quirk!
+		assert_equal( ['',25.0], parse_num( '0b10.1e1' ))
+		assert_equal( ['',81.25], parse_num( '0o10.1e1' ))
+		assert_equal( ['',81.25], parse_num( '010.1e1' ))
+
+		assert_equal( ['',32.125], parse_num( '0x10.1p1' ))
+		assert_equal( ['',5.0], parse_num( '0b10.1p1' ))
+		assert_equal( ['',16.25], parse_num( '0o10.1p1' ))
+		assert_equal( ['',16.25], parse_num( '010.1p1' ))
+	end
+### >>>
+	def test_floats_nve
+		assert_equal( ['-1',0.0], parse_num( '0x10e-1' )) # quirk!
+		assert_equal( ['',0.0], parse_num( '0b10e-1' ))
+		assert_equal( ['',0.0], parse_num( '0o10e-1' ))
+		assert_equal( ['',0.0], parse_num( '010e-1' ))
+
+		assert_equal( ['',0.0], parse_num( '0x10p-1' ))
+		assert_equal( ['',0.0], parse_num( '0b10p-1' ))
+		assert_equal( ['',0.0], parse_num( '0o10p-1' ))
+		assert_equal( ['',0.0], parse_num( '010p-1' ))
+
+		assert_equal( ['-1',0.0], parse_num( '0x10.1e-1' )) # quirk!
+		assert_equal( ['',0.0], parse_num( '0b10.1e-1' ))
+		assert_equal( ['',0.0], parse_num( '0o10.1e-1' ))
+		assert_equal( ['',0.0], parse_num( '010.1e-1' ))
+
+		assert_equal( ['',0.0], parse_num( '0x10.1p-1' ))
+		assert_equal( ['',0.0], parse_num( '0b10.1p-1' ))
+		assert_equal( ['',0.0], parse_num( '0o10.1p-1' ))
+		assert_equal( ['',0.0], parse_num( '010.1p-1' ))
+	end
+	def test_floats_pve
+		assert_equal( ['+1',0.0], parse_num( '0x10e+1' )) # quirk!
+		assert_equal( ['',0.0], parse_num( '0b10e+1' ))
+		assert_equal( ['',0.0], parse_num( '0o10e+1' ))
+		assert_equal( ['',0.0], parse_num( '010e+1' ))
+
+		assert_equal( ['',0.0], parse_num( '0x10p+1' ))
+		assert_equal( ['',0.0], parse_num( '0b10p+1' ))
+		assert_equal( ['',0.0], parse_num( '0o10p+1' ))
+		assert_equal( ['',0.0], parse_num( '010p+1' ))
+
+		assert_equal( ['+1',0.0], parse_num( '0x10.1e+1' )) # quirk!
+		assert_equal( ['',0.0], parse_num( '0b10.1e+1' ))
+		assert_equal( ['',0.0], parse_num( '0o10.1e+1' ))
+		assert_equal( ['',0.0], parse_num( '010.1e+1' ))
+
+		assert_equal( ['',0.0], parse_num( '0x10.1p+1' ))
+		assert_equal( ['',0.0], parse_num( '0b10.1p+1' ))
+		assert_equal( ['',0.0], parse_num( '0o10.1p+1' ))
+		assert_equal( ['',0.0], parse_num( '010.1p+1' ))
+	end
 end
